9695088fa8dc7e0b24d3cc598ba64ca4b151f06c
bennylp <benny@stosia.com>
2025-02-04 13:48:54 +0700

    Fix installlinux.sh when it is run from outside directory

--- installlinux.sh
+++ installlinux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Note: this script may be run from user's root repo directory
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "$SCRIPT_DIR"
+
 echo Installing gdb..
 sudo apt-get -y -q install gdb
 sudo `which python` linuxrunner.py -i
+
+popd
